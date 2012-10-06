a9a4b15c99c98b6d7ed2a7c7b3760a21dd60cb69
corinna <corinna@ubuntu>
2012-10-05 23:05:22 -0400

    switch to php files so the includes work on ipage's server

--- index.php
+++ index.php
@@ -1,50 +1,57 @@
-<!--#include virtual="/template/header.html" -->
-<link rel="stylesheet" href="/css/liteaccordion.css">
-<!--[if lt IE 9]>
-<script>
-    document.createElement('figure');
-    document.createElement('figcaption');           
-</script>
-<![endif]-->
-<style>
-#accordian {
-    font: 14px/24px 'Lucida Sans Unicode', 'Bitstream Vera Sans',
-        'Trebuchet Unicode MS', 'Lucida Grande', Verdana, Helvetica,
-        sans-serif;
-    color: #333;
-    margin: auto;
-}
-
-figure {
-    display: block;
-    width: 100%;
-    height: 100%;
-    margin: 0;
-}
-
-figcaption {
-    padding: 10px 15px;
-    position: absolute;
-    bottom: 20px;
-    right: 30px;
-    z-index: 3;
-    background: black;
-    background: rgba(0, 0, 0, 0.5);
-    color: white;
-    -webkit-border-radius: 4px;
-    -moz-border-radius: 4px;
-    border-radius: 4px;
-    width: 300px;
-}
-</style>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Embody Christ Fellowship</title>
+    <link rel="stylesheet" href="/css/liteaccordion.css">
+    <!--[if lt IE 9]>
+    <script>
+        document.createElement('figure');
+        document.createElement('figcaption');           
+    </script>
+    <![endif]-->
+    <style>
+    #accordian {
+        font: 14px/24px 'Lucida Sans Unicode', 'Bitstream Vera Sans',
+            'Trebuchet Unicode MS', 'Lucida Grande', Verdana, Helvetica,
+            sans-serif;
+        color: #333;
+        margin: auto;
+    }
+    
+    figure {
+        display: block;
+        width: 100%;
+        height: 100%;
+        margin: 0
+    }
+    
+    figcaption {
+        padding: 10px 15px;
+        position: absolute;
+        bottom: 20px;
+        right: 30px;
+        z-index: 3;
+        background: black;
+        background: rgba(0, 0, 0, 0.5);
+        color: white;
+        -webkit-border-radius: 4px;
+        -moz-border-radius: 4px;
+        border-radius: 4px;
+        width: 300px;
+    }
+    </style>
+    <?php include "template/header.php" ?>
+</head>
+<body>
 <section id="content">
     <h2 id="title">Welcome to Embody Christ Fellowship</h2>
     Embody is a new church and has found a permanent location not far from you (2014 South Shore Dr.). 
-    We are a church where you don’t have to believe to belong. 
+    We are a church where you don't have to believe to belong. 
     Everyone is at a different place in faith, and we are a fellowship of people who want to seek after 
-    the life that Jesus taught about. We don’t have all the answers, but we’ll seek them out together.<br><br>
+    the life that Jesus taught about. We don't have all the answers, but we'll seek them out together.<br><br>
     
-    If you’re looking for an authentic, relevant place to find friendship; and if you want to go deeper 
+    If you're looking for an authentic, relevant place to find friendship; and if you want to go deeper 
     as we look at the life and teachings of Jesus...be our guest. We are just like our neighborhood: 
     singles, married, divorced, young, old, alcoholics, soccer moms, Christians, agnostics, atheists, 
     jobless, enthusiastic, depressed... we are regular people, so...no judgment here...just coffee 
@@ -122,4 +129,6 @@ figcaption {
     }).find('figcaption:first').show().end().find('figcaption:not(:first)').hide();
 })(jQuery);
 </script>
-<!--#include virtual="/template/footer.html" -->
+<?php include "template/footer.php" ?>
+</body>
+</html>
