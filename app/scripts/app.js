a668ca587254b7074fcf87871b8fc38c16247e85
Corinna  Schmidt <corinnaerin@users.noreply.github.com>
2013-01-13 20:22:57 -0500

    convert to angular

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var embodychristApp = angular.module('embodychristApp', ['ui'])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers.js
@@ -0,0 +1,9 @@
+'use strict';
+
+embodychristApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
