0a3872e929418c934830bd29fe046d2d7ad99520
Corinna  Schmidt <corinnaerin@users.noreply.github.com>
2013-02-28 21:04:08 -0500

    fix accordian to work with angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,36 @@
 'use strict';
 
 var embodychristApp = angular.module('embodychristApp', ['ui'])
-  .config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  }]);
+    .config(['$routeProvider', function($routeProvider) {
+      $routeProvider
+        .when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+    }])
+    .directive('accordian', function factory() {
+          return function postLink($scope) {
+              $('#accordian').liteAccordion({
+                responsive: true,
+                theme:'stitch',
+                easing:'easeOutBack',
+                rounded: true,
+                autoPlay:true,
+                onTriggerSlide : function() {
+                    var index = this.closest('li').index();
+                    $('figcaption').filter(function() {
+                        return $(this).closest('li').index() != index
+                    }).fadeOut();
+                },
+                onSlideAnimComplete : function() {    
+                    this.find('figcaption').fadeIn();
+                },
+                pauseOnHover:true,
+                activateOn: 'mouseover',
+                autoScaleImages : true
+            }).find('figcaption:first').show().end().find('figcaption:not(:first)').hide();
+        }
+    });
